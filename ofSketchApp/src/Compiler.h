ffe6398fb6e31782f5843fa8ed0fd691335e2da3
Christopher Baker <me@christopherbaker.net>
2014-08-18 11:21:30 -0500

    Server license cleanup.

--- ofSketchApp/src/Compiler.h
+++ ofSketchApp/src/Compiler.h
@@ -1,6 +1,6 @@
 // =============================================================================
 //
-// Copyright (c) 2014 Brannon Dorsey <http://christopherbaker.net>
+// Copyright (c) 2014 Brannon Dorsey <http://brannondorsey.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
--- ofSketchApp/src/ProjectManager.cpp
+++ ofSketchApp/src/ProjectManager.cpp
@@ -1,7 +1,7 @@
 // =============================================================================
 //
-// Copyright (c) 2013 Christopher Baker <http://christopherbaker.net>
-//               2014 Brannon Dorsey <http://brannondorsey.com>
+// Copyright (c) 2013-2014 Christopher Baker <http://christopherbaker.net>
+// Copyright (c) 2014 Brannon Dorsey <http://brannondorsey.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
