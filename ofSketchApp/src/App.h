528da3b916395974897f6a56fb13f084ba68687e
Brannon Dorsey <brannon@brannondorsey.com>
2014-08-31 21:08:42 -0500

    v0.3.2 Version Bump

--- ofSketchApp/src/App.h
+++ ofSketchApp/src/App.h
@@ -121,7 +121,7 @@ public:
     {
         VERSION_MAJOR = 0,
         VERSION_MINOR = 3,
-        VERSION_PATCH = 1
+        VERSION_PATCH = 2
     };
 
     static const std::string VERSION_SPECIAL;
