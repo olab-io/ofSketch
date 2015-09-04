5e5b5f7f0bdb0ff7282d0cef33413757380365bb
Christopher Baker <me@christopherbaker.net>
2015-09-03 21:31:30 -0500

    Add file filters classes.

---
+++ ofSketchApp/src/FileFilters.h
@@ -0,0 +1,67 @@
+// =============================================================================
+//
+// Copyright (c) 2014 Brannon Dorsey <http://brannondorsey.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// =============================================================================
+
+
+#pragma once
+
+
+#include "ofx/IO/AbstractTypes.h"
+#include "ofx/IO/DirectoryFilter.h"
+#include "ofx/IO/RegexPathFilter.h"
+#include "ofx/IO/PathFilterCollection.h"
+
+
+namespace of {
+namespace Sketch {
+
+
+class ProjectFilter: public ofx::IO::AbstractPathFilter
+{
+public:
+	ProjectFilter();
+	~ProjectFilter();
+
+	bool accept(const Poco::Path& path) const override;
+
+	ofx::IO::DirectoryFilter _directoryFilter;
+	ofx::IO::RegexPathFilter _regexFilter;
+	ofx::IO::PathFilterCollection _filter;
+
+};
+
+
+class ProjectFileFilter: public ofx::IO::AbstractPathFilter
+{
+public:
+	ProjectFileFilter();
+	~ProjectFileFilter();
+
+	bool accept(const Poco::Path& path) const override;
+	
+	
+};
+
+
+
+} } // namespace of::Sketch
